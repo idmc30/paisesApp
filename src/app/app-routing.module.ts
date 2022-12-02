0678859faf0706f426d801d8675942fec4d79918
idmc <id.morales10n@gmail.com>
2022-12-02 08:01:26 -0500

    router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path:'',
     component: PorPaisComponent,
-    pathMatch: 'full'
+    pathMatch: 'full'   //cuando en la url ingresa vacio
   },
   {
     path:'region',
